e6ac3a0434dbf438343c5b145797e5d1a314f6f2
Eugene Zhdanov <euegenezhdanov1@gmail.com>
2025-02-10 05:07:45 +0200

    feat(router): add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import "./App.css";
 import stickerGif from "./assets/sticker.gif";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-function App() {
+function MainPage() {
   const [isLoading, setIsLoading] = useState(false);
   const [isSuccess, setIsSuccess] = useState(false);
   const [isError, setIsError] = useState(false);
@@ -54,15 +55,22 @@ function App() {
   return (
     <div>
       <h1>Вы любите спать и кушать?</h1>
-      <button
-        onClick={sendUserDataHandler}
-        className="button"
-        disabled={isLoading}
-        
-      >
+      <button onClick={sendUserDataHandler} className="button" disabled={isLoading}>
         {isLoading ? "Отправляем ответ..." : "Да"}
+        {isLoading && <div className="button-spinner" />}
       </button>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/main" element={<MainPage />} />
+        <Route path="*" element={<Navigate to="/main" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
