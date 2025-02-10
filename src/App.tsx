d9c5ce9c1f2f66fe8568528372494552b50d2b3b
Eugene Zhdanov <euegenezhdanov1@gmail.com>
2025-02-10 04:10:06 +0200

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,67 @@
+import { useState } from "react";
+
 import "./App.css";
 import stickerGif from "./assets/sticker.gif";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const sendUserDataHandler = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(`${API_URL}/user`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          telegramId: 123456789,
+          username: "testUser",
+        }),
+      });
+
+      const data = await response.json();
+      if (data.message === "success") {
+        setIsSuccess(true);
+      }
+    } catch (error) {
+      console.error("Ошибка при отправке данных:", error);
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  if (isSuccess) {
+    return (
+      <div>
+        <h1>Поздравляем! Вы хорошая обезьяна!</h1>
+        <div className="gif-container">
+          <img src={stickerGif} alt="sticker" />
+        </div>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return <div>Что-то сломалось, попробуйте позже.</div>;
+  }
+
   return (
     <div>
-      <h1>Hello World</h1>
-      <div className="gif-container">
-        <img src={stickerGif} alt="sticker" />
-      </div>
+      <h1>Вы любите спать и кушать?</h1>
+      <button
+        onClick={sendUserDataHandler}
+        className="button"
+        disabled={isLoading}
+        
+      >
+        {isLoading ? "Отправляем ответ..." : "Да"}
+      </button>
     </div>
   );
 }
